4af375798b9f036627e265beca70d62f4d5057a8
Snabrun <egor.shkaroburov@edu.keuda.fi>
2023-02-01 12:38:05 +0200

    Add project files.

---
+++ 5. Harjoitus - koodi ja video/Form1.cs
@@ -0,0 +1,20 @@
+namespace _5._Harjoitus___koodi_ja_video
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
