678e6d2191669b8400c97f5acfb7ccfe2b18607b
Snabrun <egor.shkaroburov@edu.keuda.fi>
2023-02-01 15:14:38 +0200

    H5

--- 5. Harjoitus - koodi ja video/Form1.Designer.cs
+++ 5. Harjoitus - koodi ja video/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.uusiLukuTB = new System.Windows.Forms.TextBox();
+            this.vastausTB = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +43,32 @@
             this.label1.Text = "Anna luku (-999) lopetus";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // uusiLukuTB
+            // 
+            this.uusiLukuTB.Location = new System.Drawing.Point(165, 68);
+            this.uusiLukuTB.Name = "uusiLukuTB";
+            this.uusiLukuTB.Size = new System.Drawing.Size(100, 23);
+            this.uusiLukuTB.TabIndex = 1;
+            this.uusiLukuTB.TextChanged += new System.EventHandler(this.uusiLukuTB_TextChanged);
+            this.uusiLukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.uusiLukuTB_KeyPress);
+            // 
+            // vastausTB
+            // 
+            this.vastausTB.AutoSize = true;
+            this.vastausTB.Location = new System.Drawing.Point(21, 124);
+            this.vastausTB.Name = "vastausTB";
+            this.vastausTB.Size = new System.Drawing.Size(38, 15);
+            this.vastausTB.TabIndex = 2;
+            this.vastausTB.Text = "label2";
+            this.vastausTB.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(452, 169);
+            this.Controls.Add(this.vastausTB);
+            this.Controls.Add(this.uusiLukuTB);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,5 +81,7 @@
         #endregion
 
         private Label label1;
+        private TextBox uusiLukuTB;
+        private Label vastausTB;
     }
 }
--- 5. Harjoitus - koodi ja video/Form1.cs
+++ 5. Harjoitus - koodi ja video/Form1.cs
@@ -2,6 +2,7 @@ namespace _5._Harjoitus___koodi_ja_video
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
@@ -9,12 +10,55 @@ namespace _5._Harjoitus___koodi_ja_video
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+        List<int> jono = new List<int>();
+        private void uusiLukuTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (uusiLukuTB.Text == "-999")
+                {
+                    vastausTB.Text = "";  
+                    int[] taulukko = jono.ToArray();
+                    Array.Sort(taulukko);
+                    foreach (var jasen in taulukko)
+                    {
+                      vastausTB.Text += jasen + " ";
+                    }
+                    vastausTB.Visible = true;
+                }
+
+            }
+            else
+            {               
+                jono.Add(Int32.Parse(uusiLukuTB.Text));
+                uusiLukuTB.Text = "";
+                
+            }
+
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                        TyhjaaLomake();
+            }
+
+            
+        }
+        
+        private void TyhjaaLomake()
+        {
+            uusiLukuTB.Text = "";
+        }
+    
+
+        private void uusiLukuTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+       
 }
