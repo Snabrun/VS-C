5600b1334d0313042d8ca9d953bf512d265b198d
Snabrun <egor.shkaroburov@edu.keuda.fi>
2023-02-08 13:17:59 +0200

    Noppa peli

---
+++ Tehtävät 11-12/Tehtävät 11-12/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Tehtävät_11_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.noppa01PB = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.noppa02PB = new System.Windows.Forms.PictureBox();
+            this.heitaBT = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa01PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa02PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // noppa01PB
+            // 
+            this.noppa01PB.Image = global::Tehtävät_11_12.Properties.Resources.dice01;
+            this.noppa01PB.Location = new System.Drawing.Point(23, 96);
+            this.noppa01PB.Name = "noppa01PB";
+            this.noppa01PB.Size = new System.Drawing.Size(100, 100);
+            this.noppa01PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.noppa01PB.TabIndex = 0;
+            this.noppa01PB.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Swis721 Blk BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(37, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nopan heitto";
+            // 
+            // noppa02PB
+            // 
+            this.noppa02PB.Image = global::Tehtävät_11_12.Properties.Resources.dice01;
+            this.noppa02PB.Location = new System.Drawing.Point(146, 96);
+            this.noppa02PB.Name = "noppa02PB";
+            this.noppa02PB.Size = new System.Drawing.Size(100, 100);
+            this.noppa02PB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.noppa02PB.TabIndex = 2;
+            this.noppa02PB.TabStop = false;
+            // 
+            // heitaBT
+            // 
+            this.heitaBT.Font = new System.Drawing.Font("Swis721 BlkCn BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.heitaBT.Location = new System.Drawing.Point(23, 221);
+            this.heitaBT.Name = "heitaBT";
+            this.heitaBT.Size = new System.Drawing.Size(223, 48);
+            this.heitaBT.TabIndex = 3;
+            this.heitaBT.Text = "Heitä";
+            this.heitaBT.UseVisualStyleBackColor = true;
+            this.heitaBT.Click += new System.EventHandler(this.heitaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 289);
+            this.Controls.Add(this.heitaBT);
+            this.Controls.Add(this.noppa02PB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.noppa01PB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.noppa01PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.noppa02PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox noppa01PB;
+        private Label label1;
+        private PictureBox noppa02PB;
+        private Button heitaBT;
+    }
+}
