2a1cffdfcc8ae849f4e5a4c4d18c47809d56eaba
Snabrun <egor.shkaroburov@edu.keuda.fi>
2023-02-28 22:17:31 +0200

    Roomalaiset numerot

---
+++ Teht 7- 8/Roomalaiset nrot/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Roomalaiset_nrot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.muutaBT = new System.Windows.Forms.Button();
+            this.vastausLB = new System.Windows.Forms.Label();
+            this.tekstiTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // muutaBT
+            // 
+            this.muutaBT.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.muutaBT.Location = new System.Drawing.Point(283, 157);
+            this.muutaBT.Name = "muutaBT";
+            this.muutaBT.Size = new System.Drawing.Size(100, 34);
+            this.muutaBT.TabIndex = 0;
+            this.muutaBT.Text = "Muuta";
+            this.muutaBT.UseVisualStyleBackColor = true;
+            this.muutaBT.Click += new System.EventHandler(this.muutaBT_Click);
+            // 
+            // vastausLB
+            // 
+            this.vastausLB.AutoSize = true;
+            this.vastausLB.Font = new System.Drawing.Font("Unispace", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.vastausLB.Location = new System.Drawing.Point(112, 138);
+            this.vastausLB.Name = "vastausLB";
+            this.vastausLB.Size = new System.Drawing.Size(51, 25);
+            this.vastausLB.TabIndex = 1;
+            this.vastausLB.Text = "---";
+            this.vastausLB.Visible = false;
+            // 
+            // tekstiTB
+            // 
+            this.tekstiTB.BackColor = System.Drawing.Color.LightCyan;
+            this.tekstiTB.Location = new System.Drawing.Point(283, 128);
+            this.tekstiTB.Name = "tekstiTB";
+            this.tekstiTB.Size = new System.Drawing.Size(100, 23);
+            this.tekstiTB.TabIndex = 2;
+            this.tekstiTB.TextChanged += new System.EventHandler(this.tekstiTB_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(33, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Roomalaiset numerot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Unispace", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(49, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(334, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Anna numero välillä 1-3999 ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(426, 227);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tekstiTB);
+            this.Controls.Add(this.vastausLB);
+            this.Controls.Add(this.muutaBT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button muutaBT;
+        private Label vastausLB;
+        private TextBox tekstiTB;
+        private Label label1;
+        private Label label2;
+    }
+}
