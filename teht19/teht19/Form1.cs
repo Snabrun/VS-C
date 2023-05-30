15e9471b970aee61f30717a7aab4fef35318c73f
Egor Shkaroburov <egor.shkaroburov@edu.keuda.fi>
2023-05-31 00:05:33 +0300

    teht 19

---
+++ teht19/teht19/Form1.cs
@@ -0,0 +1,76 @@
+﻿namespace teht19
+{
+    public partial class ruokalistaForm : Form
+    {
+        public ruokalistaForm()
+        {
+            InitializeComponent();
+            meistaPL.Visible = true;
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = true;
+            herkutPL.Visible = false;
+            koriPL.Visible = false;
+        }
+
+
+
+        private void ruokalistaForm_Load(object sender, EventArgs e)
+        {
+            meistaLB.Text = "Keudan oppilaskunnan palvelee luokassa 320 valituntisin \n";
+            meistaLB.Text += "9:00 - 14:30 joka päivä maanantaisin ja perjantaisin  \n";
+            meistaLB.Text += "9:00 - 12:30.  \n \n Keudan oppilaskunnan kahvilasta saa lämpimien \n";
+            meistaLB.Text += "juomien lisäksi virvokkeita sekä pientä purtavaa ja makeisia";
+            meistaLB.Text += new Font("Arial", 12);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = true;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = false;
+            herkutPL.Visible = false;
+            koriPL.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = true;
+            juomatPL.Visible = false;
+            herkutPL.Visible = false;
+            koriPL.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = false;
+            herkutPL.Visible = true;
+            koriPL.Visible = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            meistaPL.Visible = false;
+            ruoatPL.Visible = false;
+            juomatPL.Visible = false;
+            herkutPL.Visible = false;
+            koriPL.Visible = true;
+        }
+
+        private void exitBT_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
